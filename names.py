426eef6d4c4b9ac2ebeccbeea5bea3a52fbd15f3
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-07-30 00:37:13 -0300

    Update and rename username.py to names.py

---
+++ names.py
@@ -0,0 +1,2 @@
+first_names = ['Enzo', 'Caio', 'Antonio Daniel', 'Pedro Henrique', 'Raphael', 'Vitor', 'Xande', 'Fernanda', 'Rodrigo', 'Nora', 'Mark', 'Jeff', 'Elon', 'Bill', 'Warren', 'Bernard']
+last_names = ['Moraes', 'Peixoto', 'Hermann', 'Salles', 'Fonseca', 'Cabral', 'Fracassado', 'Fracassada', 'Fracasso', 'Nao deu Bom', 'Zuckerberg', 'Bezos', 'Musk', 'Gates', 'Buffett', 'Arnault']
