dc428e77f8771ec5eb06b70ecb23d5a0a2c2fe4b
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-07-31 00:46:28 -0300

    Update form_Proton.py

--- form_Proton.py
+++ form_Proton.py
@@ -1,74 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from lists import first_names, last_names, mail_usernames
+from lists import mail_usernames
 from mail_verification import get_code
 from time import sleep
 from random import randint
 
 
-def get_random(digits):
-    rdm = ''
-    for i in range(0, digits):
-        rdm += str(randint(0, 9))
-    return rdm
+def get_email(username, password):
+    driver = webdriver.Chrome(r'C:\Users\felip\PycharmProjects\python_projects\Twitter Bot\chromedriver.exe')
+    driver.get('https://mail.protonmail.com/create/new?language=en')
 
+    sleep(5)
+    driver.switch_to.frame(0)
 
-def get_username(name, surname):
-    nm = name.lower().replace(' ', '')
-    sn = surname.lower().replace(' ', '')
-    return nm + sn + id_
+    driver.find_element_by_id('username').send_keys(username)
 
+    driver.switch_to.default_content()
 
-def get_password():
-    return 'G3n3r@l' + id_
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_id('passwordc').send_keys(password)
 
+    driver.switch_to.frame(1)
 
-driver = webdriver.Chrome(r'C:\Users\felip\PycharmProjects\python_projects\Twitter Bot\chromedriver.exe')
-driver.get('https://mail.protonmail.com/create/new?language=en')
+    driver.find_element_by_id('notificationEmail').send_keys('generalmarkin@yahoo.com')  # email de notificações
 
-id_ = get_random(5)
+    driver.find_element_by_name('submitBtn').click()
 
-sleep(5)
-driver.switch_to.frame(0)
+    sleep(5)
 
-username = get_username(first_names[randint(0, len(first_names) - 1)],
-                        last_names[randint(0, len(last_names) - 1)])
-driver.find_element_by_id('username').send_keys(username)
+    driver.find_element_by_id('id-signup-radio-email').click()
 
-driver.switch_to.default_content()
-
-password = get_password()
-driver.find_element_by_id('password').send_keys(password)
-driver.find_element_by_id('passwordc').send_keys(password)
-
-driver.switch_to.frame(1)
-
-driver.find_element_by_id('notificationEmail').send_keys('generalmarkin@yahoo.com')  # email de notificações
-
-driver.find_element_by_name('submitBtn').click()
-
-sleep(5)
-
-driver.find_element_by_id('id-signup-radio-email').click()
-
-email_verification = driver.find_element_by_id('emailVerification')
-mail_username = mail_usernames[randint(0, len(mail_usernames) - 1)]
-
-
-email_verification.send_keys('{}@yahoo.com'.format(mail_username))  # email para receber código de verificação
-email_verification.send_keys(Keys.RETURN)
-
-sleep(0.5)
-
-while driver.find_element_by_xpath('//*[@id="verification-panel"]/form[1]/div[2]/p/strong').text != 'Verification code sent':
-    email_verification.clear()
+    email_verification = driver.find_element_by_id('emailVerification')
     mail_username = mail_usernames[randint(0, len(mail_usernames) - 1)]
+
     email_verification.send_keys('{}@yahoo.com'.format(mail_username))  # email para receber código de verificação
     email_verification.send_keys(Keys.RETURN)
 
     sleep(0.5)
 
-sleep(15)
+    while driver.find_element_by_xpath(
+            '//*[@id="verification-panel"]/form[1]/div[2]/p/strong').text != 'Verification code sent':
+        email_verification.clear()
+        mail_username = mail_usernames[randint(0, len(mail_usernames) - 1)]
+        email_verification.send_keys('{}@yahoo.com'.format(mail_username))  # email para receber código de verificação
+        email_verification.send_keys(Keys.RETURN)
 
-driver.find_element_by_xpath('//*[@id="codeValue"]').send_keys(get_code(mail_username))
-driver.find_element_by_xpath('//*[@id="verification-panel"]/p[3]/button').click()
+        sleep(0.5)
+
+    sleep(15)
+
+    driver.find_element_by_xpath('//*[@id="codeValue"]').send_keys(get_code(mail_username))
+    driver.find_element_by_xpath('//*[@id="verification-panel"]/p[3]/button').click()
