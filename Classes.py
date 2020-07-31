690fdca17e97c83c1ab9970fb87b27b6f74f03bf
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-07-31 19:44:20 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,10 +1,14 @@
+from new_user import get_username, get_password
+
+
 class User:
-    def __init__(self, id_, name, surname, username, password):
-        self.id = id_
+    def __init__(self, id_, name, surname):
+        self.id_ = id_
         self.name = name
         self.surname = surname
-        self.username = username
-        self.password = password
+        self.username = get_username(self.name, self.surname, self.id_)
+        self.password = get_password(self.id_)
+        self.email = '{}@protonmail.com'.format(self.username)
 
-    def get_email(self):
-        return '{}@protonmail.com'.format(self.username)
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
