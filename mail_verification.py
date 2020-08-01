aae287fa4cfff1c004f9a4c4b030bcbd1b21d9ad
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-08-01 02:37:03 -0300

    Update mail_verification.py

--- mail_verification.py
+++ mail_verification.py
@@ -3,7 +3,7 @@ import re
 from time import sleep
 
 
-def get_code(username):
+def get_yahoo_code(username):
     driver = webdriver.Chrome(r'C:\Users\felip\PycharmProjects\python_projects\Twitter Bot\chromedriver.exe')
     driver.get('https://login.yahoo.com/')
 
@@ -17,10 +17,26 @@ def get_code(username):
 
     sleep(3)
 
-    driver.find_element_by_xpath('//*[@id="header-nav-bar"]/li[1]/a').click()
+    driver.find_element_by_xpath('//*[@id="header-nav-bar"]/li[1]/a').click()  # indo para o yahoo mail
 
     sleep(3)
 
-    code = re.findall(r'is: (.+?)<', driver.page_source)[0]
+    code = re.findall(r'is: (.+?)<', driver.page_source)[0]  # selecionando o último código
+
+    return code
+
+
+def get_proton_code(username, password):
+    driver = webdriver.Chrome(r'C:\Users\felip\PycharmProjects\python_projects\Twitter Bot\chromedriver.exe')
+    driver.get('https://mail.protonmail.com/login')
+
+    driver.find_element_by_id('username').send_keys(username)
+    driver.find_element_by_id('password').send_keys(password)
+
+    driver.find_element_by_id('login_btn').click()
+
+    sleep(6)
+
+    code = re.findall(r'([0-9]+) é seu', driver.page_source)[0]
 
     return code
